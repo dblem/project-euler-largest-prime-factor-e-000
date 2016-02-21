9e056a773a46aa9c163af80b58c7e1f95072b627
dblem <dax.blem@gmail.com>
2016-02-21 05:52:02 +0000

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,24 @@
-# Enter your procedural solution here!
+def largest_prime_factor(input)
+
+  input = input.to_i
+  input_root = Math.sqrt(input).to_i
+  largest_prime = 0
+
+  def is_prime?(num)
+    num_root = Math.sqrt(num).to_i
+    (2..num_root).all? do |x|
+      num % x != 0
+    end
+  end
+
+  input_root.downto(1) do |num|
+    if largest_prime == 0
+      largest_prime = num if input % num == 0 && is_prime?(num) == true
+    else
+      break
+    end
+  end
+
+  largest_prime
+
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,27 @@
-# Enter your object-oriented solution here!
+class LargestPrimeFactor
+  
+  def initialize(input)
+    @input = input.to_i
+    @input_root = Math.sqrt(input).to_i
+    @largest_prime = 0
+  end
+
+  def is_prime?(num)
+    num_root = Math.sqrt(num).to_i
+    (2..num_root).all? do |x|
+      num % x != 0
+    end
+  end
+  
+  def number
+    @input_root.downto(1) do |num|
+      if @largest_prime == 0
+        @largest_prime = num if @input % num == 0 && is_prime?(num) == true
+      else
+        break
+      end
+    end
+    @largest_prime
+  end
+  
+end
